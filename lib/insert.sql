19d8e98aac6a3528808bf50cdcee348fb043db09
chadwin-uhuru <uhuruchadwin166@gmail.com>
2025-06-14 13:01:14 +0300

    Complete Organizing Bears Lab with passing tests

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 4, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 7, 'F', 'White', 'Playful', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Gruff', 1),
+('Melissa', 5, 'F', 'Black', 'Friendly', 1),
+('Grinch', 3, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 6, 'F', 'Brown', 'Gentle', 1),
+(NULL, 2, 'F', 'Black', 'Shy', 1);
